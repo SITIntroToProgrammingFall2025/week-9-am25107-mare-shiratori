7348507fa890ec3704633d037f1cc07fa3277971
AM25107-MARE-SHIRATORI <am25107@sic.shibaura-it.ac.jp>
2025-12-04 13:40:47 +0900

    hw9-1.c

--- hw9-1.c
+++ hw9-1.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main(void){
+    int a[2][2];
+    int i, j;
+    for (i=0; i<2; i++){
+        for (j=0; j<2; j++){
+            scanf("%d", &a[i][j]);}}
+    printf("You entered\n");
+    for (i=0; i<2; i++){
+        for (j=0; j<2; j++){
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+    double ans = a[0][0]*a[1][1] - a[0][1]*a[1][0];
+    printf("Determinant is %f\n", ans);
+    return 0;
+}
