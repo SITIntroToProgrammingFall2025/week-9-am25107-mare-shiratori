86967f360d00456c8eca965a61534ff9f69d73f3
AM25107-MARE-SHIRATORI <am25107@sic.shibaura-it.ac.jp>
2025-12-04 13:42:56 +0900

    ahw9-2.c

--- ahw9-2.c
+++ ahw9-2.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+
+int main(void){
+    int a[5];
+    int i, n=5;
+    printf("Input array:\n");
+    for (i=0; i<5; i++){
+        scanf("%d", &a[i]);
+    }
+    for (int i=0; i < n-1; i++) {
+        for (int j=0; j < n-1-i; j++) {
+            if (a[j] > a[j+1]) {
+                int tmp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = tmp;
+            }}}
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
 
